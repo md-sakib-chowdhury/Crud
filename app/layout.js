eb29e3d0478442b8fb3daff3e92b90e5ed82c8e9
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-02-16 19:13:03 +0600

    add topic

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
         <div className="max-w-3xl mx-auto p-4">
         <Navbar />
 
-          {children}
+         <div className="mt-8" >{children}</div>
           </div>
       
       </body>
