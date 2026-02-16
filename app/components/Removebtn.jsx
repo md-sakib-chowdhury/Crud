384ded176be7163c1c6c7bbe40db270ef6942987
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-02-16 11:46:36 +0600

    new commit

--- app/components/Removebtn.jsx
+++ app/components/Removebtn.jsx
@@ -1,7 +1,14 @@
+import {HiOutlineTrash}from "react-icons/hi";
+
 export default function Removebtn ()
 { 
-    return <div>
-        Removebtn
-    </div>
+    return (
+<button className="text-red-400">
+    <HiOutlineTrash size={24} />
+</button>
 
+
+
+    );
+        
 }
