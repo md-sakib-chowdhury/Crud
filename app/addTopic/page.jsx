de063dcd8e492a0165687cae1af6a5d9e215723e
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-02-16 18:49:42 +0600

    new commit

---
+++ app/addTopic/page.jsx
@@ -0,0 +1,4 @@
+export default function AddTopic(){
+
+    return <div>addTopic</div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import TopicsList from "./components/TopicsList"
 
 export default function Home() {
- return <TopicsList />;
+    return (
+        <>
 
- 
+            <TopicsList />
+            <TopicsList />
+
+        </>
+    );
 }
