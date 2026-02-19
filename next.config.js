b9198602eb52e12b9211370a74b3387105653372
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-02-19 13:50:17 +0600

    fix commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // Allow all hostnames
+        pathname: "/**", // Allow all paths
+      },
+    ],
+  },
+};
